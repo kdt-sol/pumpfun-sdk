27d46ed71ae318e3b5e054b312c89cadad4e0e04
DiepPk <kdt310722@gmail.com>
2025-08-11 12:35:14 +0700

    feat!: update idl

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-export default import('@kdt310722/eslint-config').then((m) => m.defineFlatConfig({}, { ignores: ['**/generated'] }, {
+export default import('@kdt310722/eslint-config').then((m) => m.defineFlatConfig({}, { ignores: ['**/generated', 'src/test.ts'] }, {
     rules: {
         'sonarjs/no-selector-parameter': 'off',
     },
